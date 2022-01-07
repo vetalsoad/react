815fb388f5cc61b66bf40685b04581dec1fc9138
vetal <vetalsoad@gmail.com>
2022-01-07 18:10:12 +0200

    323

--- less1/task2/index.js
+++ less1/task2/index.js
@@ -22,16 +22,12 @@ renderGreeting(rootElement); */
 const elem = React.createElement(
   "div",
   { className: "greeting" },
-    React.createElement(
-      "h1",
-      { className: "greeting__title" },
-      "Hello, world!"
-    ),
-    React.createElement(
-      "p",
-      { className: "greeting__text" },
-      "I'm learning React"
-    ),
+  React.createElement("div", { className: "greeting__title" }, "Hello, world!"),
+  React.createElement(
+    "div",
+    { className: "greeting__text" },
+    "I'm learning React"
+  )
 );
 
 ReactDOM.render(elem, rootElement);
