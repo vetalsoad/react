f584c07c8fe51167d2db8613bd40b2a1ada1401e
vetal <vetalsoad@gmail.com>
2022-01-07 18:12:54 +0200

    sa

--- less1/task2/index.js
+++ less1/task2/index.js
@@ -1,33 +1,7 @@
 const rootElement = document.querySelector("#root");
 
-/* const renderGreeting = (elem) => {
-  const containerElem = document.createElement("div");
-  containerElem.classList.add("greeting");
-
-  const titleElem = document.createElement("h1");
-  titleElem.classList.add("greeting__title");
-  titleElem.textContent = "Hello, world!";
-
-  const textElem = document.createElement("p");
-  textElem.classList.add("greeting__text");
-  textElem.textContent = "I'm learning React";
-
-  containerElem.append(titleElem, textElem);
-
-  elem.append(containerElem);
-};
-
-renderGreeting(rootElement); */
-
-const elem = React.createElement(
-  "div",
-  { className: "greeting" },
+const element = React.createElement("div", { className: "greeting" }, [
   React.createElement("div", { className: "greeting__title" }, "Hello, world!"),
-  React.createElement(
-    "div",
-    { className: "greeting__text" },
-    "I'm learning React"
-  )
-);
-
-ReactDOM.render(elem, rootElement);
+  React.createElement("div", { className: "greeting__text" }, "I'm learning React"),
+]);
+ReactDOM.render(element, rootElement);
