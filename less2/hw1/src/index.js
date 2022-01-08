9d6123f99769cb84ee06194920fe1b836f32796b
vetal <vetalsoad@gmail.com>
2022-01-08 15:05:37 +0200

    QW

---
+++ less2/hw1/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './list.css';
+
+
+
+const rootElem = document.querySelector('#root');
+
+const elem = (
+  <>
+    <h1 className="title">Todo List</h1>
+    <main className="todo-list">
+      <div className="actions">
+        <input className="task-input" type="text" />
+        <button className="btn create-task-btn">Create</button>
+      </div>
+      <ul className="list"></ul>
+    </main>
+  </>
+);
+
+ReactDOM.render(elem, rootElem);
