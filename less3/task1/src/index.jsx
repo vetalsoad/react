06bccaf29b5373d2f450dbbc11c237fb5f927e8e
vetal <vetalsoad@gmail.com>
2022-01-08 17:53:32 +0200

    er

--- less3/task1/src/index.jsx
+++ less3/task1/src/index.jsx
@@ -2,21 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './index.scss';
 import './search.scss';
+import {SearchField} from './Search.jsx';
 
 const rootElement = document.querySelector("#root");
-
-const SearchField = (props) => {
-  console.log(props);
-  return (
-    <div className='search'>
-      <h1 className='search__title'>{`Hello, ${props.name}. What to search for you?`}</h1>
-      <div className='search__field'>
-        <input type='text' className='search__input' />
-        <button className='search__button'>Search</button>
-      </div>
-    </div>
-  );
-
-}
 
 ReactDOM.render(<SearchField name="Bob" />, rootElement);
