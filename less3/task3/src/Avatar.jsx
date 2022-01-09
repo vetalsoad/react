945ec4e386e4f3234b52aae584d49d1c1a876173
vetal <vetalsoad@gmail.com>
2022-01-09 14:02:05 +0200

    rew

---
+++ less3/task3/src/Avatar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UserAvatar = (props) => {
+  return (
+    <img
+      className='avatar'
+      src={props.avatarUrl}
+      alt={props.name}
+    />
+  );
+};
+
+export default UserAvatar;
