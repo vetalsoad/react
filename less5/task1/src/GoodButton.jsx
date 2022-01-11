acb980a38c5cf974622b4c0a1ebc499d514b0602
vetal <vetalsoad@gmail.com>
2022-01-11 18:36:35 +0200

    ee

---
+++ less5/task1/src/GoodButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './button.scss'
+
+const GoodButton = () => {
+  return(<button 
+    className='fancy-button'
+    onClick={() => alert('Good job!')}
+  >
+    Click me!
+  </button>
+  )
+};
+
+
+export default GoodButton;
