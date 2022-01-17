e8a7ef8f4f8dbfd4ece1bd0a9206722de074744b
vetal <vetalsoad@gmail.com>
2022-01-17 17:54:08 +0200

    wqr

--- less7/task2/src/index.jsx
+++ less7/task2/src/index.jsx
@@ -1,8 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './index.scss';
-import Auth from './Auth';
+import UsersList from "./UsersList";
+
 
 const rootElement = document.querySelector("#root");
 
-ReactDOM.render(<Auth />,rootElement);
+const users = [
+  {
+    id: "id-0",
+    age: 21,
+    name: "Bob",
+  },
+  {
+    id: "id-1",
+    age: 17,
+    name: "Tom",
+  },
+  {
+    id: "id-2",
+    age: 18,
+    name: "Tom",
+  },
+  {
+    id: "id-3",
+    age: 45,
+    name: "Sam",
+  },
+];
+
+
+ReactDOM.render(<UsersList users={users} />,rootElement);
