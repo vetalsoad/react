d88980212576fab04ebc8b53eb74ca5e1da3eb75
vetal <vetalsoad@gmail.com>
2022-01-18 18:41:13 +0200

    tyt

--- less8/task1c/src/index.jsx
+++ less8/task1c/src/index.jsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import Demo from "./Demo";
 
-
-
 const rootElement = document.querySelector("#root");
 
-ReactDOM.render(<Demo/>,rootElement);
+ReactDOM.render(<Demo />, rootElement);
