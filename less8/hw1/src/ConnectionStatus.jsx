64c55cd0c682c93658abb6de203a0688e2719687
vetal <vetalsoad@gmail.com>
2022-01-19 21:46:49 +0200

    ui

---
+++ less8/hw1/src/ConnectionStatus.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class ConnectionStatus extends Component {
+  state = {
+    status: "online",
+  };
+
+  componentDidMount() {
+    window.addEventListener("online", this.handleOnline);
+    window.addEventListener("offline", this.handleOffline);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("online", this.handleOnline);
+    window.removeEventListener("offline", this.handleOffline);
+  }
+
+  handleOnline = () => {
+    this.setState({
+      status: "online",
+    });
+  }
+
+  handleOffline = () => {
+    this.setState({
+      status: "offline",
+    });
+  }
+
+  render() {
+    return (
+      <div
+        className={`status ${
+          this.state.status === "offline" ? "status__offline" : ""
+        }`}
+      >
+        {this.state.status}
+      </div>
+    );
+  }
+}
+
+export default ConnectionStatus;
