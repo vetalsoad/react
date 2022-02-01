bd6831e4ae8bb43f22271f38d30d02e311cc8f06
vetal <vetalsoad@gmail.com>
2022-02-01 19:48:27 +0200

    wqw

--- less11/task2/src/App.jsx
+++ less11/task2/src/App.jsx
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
 
+import Dialog from "./Dialog";
 
+class App extends Component {
+  state = {
+    isOpen: false,
+  };
 
-class Auth extends Component {
-  render(){
-    return(
-      <div>hewer</div>
-    )
+  hideDialog = () => {
+    this.setState({
+      isOpen: false,
+    });
+  };
+
+  showDialog = () => {
+    this.setState({
+      isOpen: true,
+    });
+  };
+
+  render() {
+    return (
+      <div className='app'>
+        <button className='btn' onClick={this.showDialog}>
+          Show
+        </button>
+        <Dialog
+          isOpen={this.state.isOpen}
+          onClose={this.hideDialog}
+          title='some title'
+        >
+          <p>Some text</p>
+        </Dialog>
+      </div>
+    );
   }
 }
 
-export default Auth;
+export default App;
