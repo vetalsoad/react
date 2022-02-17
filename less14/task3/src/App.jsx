c0095d614a545a2f2731d84feedb4cdc808d387d
vetal <vetalsoad@gmail.com>
2022-02-17 14:54:46 +0200

    qasw

---
+++ less14/task3/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Demo from "./Demo";
+
+const App = () => {
+  return <Demo />;
+};
+
+export default App;
---
+++ less14/task3/src/Dimensions.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+const Dimensions = () => {
+  const [dimensions, setDimensions] = useState({
+    width: null,
+    height: null,
+  });
+
+  useEffect(() => {
+    const { innerHeight, innerWidth } = window;
+    setDimensions({ width: innerWidth, height: innerHeight });
+    const handleResize = event => {
+      const { innerWidth, innerHeight } = event.target;
+      setDimensions({ width: innerWidth, height: innerHeight });
+    }
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      debugger;
+      window.removeEventListener("resize", handleResize);
+    }
+  }, []);
+
+  const { width, height } = dimensions;
+  return <div className='dimensions'>{`${width}px - ${height}px`}</div>;
+};
+
+export default Dimensions;
